74751c7791e86dd24a9e37befdf69aa988721170
Blaxxun <blaxxun@blaxxun.com>
2021-05-08 19:38:23 +0200

    effects that trigger on hit

---
+++ EpicLoot/MagicItemEffects/AvoidDamageTaken.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace EpicLoot.MagicItemEffects
+{
+	[HarmonyPatch(typeof(Character), "Damage")]
+	public class AvoidDamageTaken_Character_Damage_Patch
+	{
+		[UsedImplicitly]
+		private static bool Prefix(Character __instance, HitData hit)
+		{
+			Character attacker = hit.GetAttacker();
+			if (__instance is Player player && player.HasMagicEquipmentWithEffect(MagicEffectType.AvoidDamageTaken) && attacker != null && attacker != __instance)
+			{
+				var avoidanceChance = 0f;
+				var items = player.GetMagicEquipmentWithEffect(MagicEffectType.AvoidDamageTaken);
+				foreach (var item in items)
+				{
+					avoidanceChance += item.GetMagicItem().GetTotalEffectValue(MagicEffectType.AvoidDamageTaken, 0.01f);
+				}
+
+				return !(Random.Range(0f, 1f) < avoidanceChance);
+			}
+
+			return true;
+		}
+	}
+}
