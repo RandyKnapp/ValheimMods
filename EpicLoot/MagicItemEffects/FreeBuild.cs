ce39413a7a4704b8aa123c08dbeb439670c457af
Blaxxun <blaxxun@blaxxun.com>
2021-05-08 23:16:35 +0200

    new magic effect: free build

---
+++ EpicLoot/MagicItemEffects/FreeBuild.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace EpicLoot.MagicItemEffects
+{
+	[HarmonyPatch]
+	public class FreeBuildGuiDisplay_Recipe_GetRequiredStation_Patch
+	{
+		[UsedImplicitly]
+		private static IEnumerable<MethodBase> TargetMethods()
+		{
+			yield return AccessTools.DeclaredMethod(typeof(Player), nameof(Player.HaveRequirements), new[] {typeof(Piece), typeof(Player.RequirementMode)});
+			yield return AccessTools.DeclaredMethod(typeof(Player), nameof(Player.CheckCanRemovePiece));
+			yield return AccessTools.DeclaredMethod(typeof(Hud), nameof(Hud.SetupPieceInfo));
+		}
+
+		[UsedImplicitly]
+		private static void Prefix(ref CraftingStation __state, Piece piece)
+		{
+			if (piece == null)
+			{
+				return;
+			}
+
+			__state = piece.m_craftingStation;
+			if (Player.m_localPlayer.HasMagicEquipmentWithEffect(MagicEffectType.FreeBuild))
+			{
+				piece.m_craftingStation = null;
+			}
+		}
+
+		[UsedImplicitly]
+		private static void Postfix(CraftingStation __state, Piece piece)
+		{
+			if (piece != null)
+			{
+				piece.m_craftingStation = __state;
+			}
+		}
+	}
+}
