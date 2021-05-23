d47d33f4b3119b2e62aef6cf240679ee2a8e7ab6
Blaxxun <blaxxun@blaxxun.com>
2021-05-23 20:15:28 +0200

    store edif data in zdo

---
+++ ExtendedItemDataFramework/Humanoid_Patch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+
+namespace ExtendedItemDataFramework
+{
+    public static class Humanoid_Patch
+    {
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.EquipItem))]
+        class AttachExtendedDataToZDO_Humanoid_EquipItem_Patch
+        {
+            private static void Postfix(ItemDrop.ItemData item, Humanoid __instance)
+            {
+                if (__instance == Player.m_localPlayer)
+                {
+                    item.Extended()?.UpdatePlayerZDOForEquipment();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.UnequipItem))]
+        class AttachExtendedDataToZDO_Humanoid_UnequipItem_Patch
+        {
+            private static void Postfix(ItemDrop.ItemData item, Humanoid __instance)
+            {
+                if (__instance == Player.m_localPlayer)
+                {
+                    item.Extended()?.UpdatePlayerZDOForEquipment(false);
+                }
+            }
+        }
+    }
+}
