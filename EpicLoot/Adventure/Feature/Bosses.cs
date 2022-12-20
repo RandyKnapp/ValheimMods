0a7a35d1c521ddb4333113028bafe96b83d897b0
Pete Navarra <vapokrocks@gmail.com>
2022-12-20 17:45:02 -0500

    Adding in feedback to the user that adventure.json has not been updated.
    (cherry picked from commit 634cbc1d67faee2cf5e42eaecca364c3b8da86e0)

--- EpicLoot/Adventure/Feature/Bosses.cs
+++ EpicLoot/Adventure/Feature/Bosses.cs
@@ -6,9 +6,17 @@ namespace EpicLoot.Adventure.Feature
     {
         public static string GetPrevBossKey(string bossKey)
         {
+            if (AdventureDataManager.Config.Bounties.Bosses == null || AdventureDataManager.Config.Bounties.Bosses.Count == 0)
+            {
+                throw new System.Exception(($"[Adventure Bounty Error]: adventure.json has not been updated or is missing boss config.  Please update JSON files."));
+
+            }
+
             var index = AdventureDataManager.Config.Bounties.Bosses.FindIndex(x => x.BossDefeatedKey == bossKey);
+
             if (index == 0 || index >= AdventureDataManager.Config.Bounties.Bosses.Count)
                 return null;
+
             return AdventureDataManager.Config.Bounties.Bosses[index - 1].BossDefeatedKey;
         }
     }
