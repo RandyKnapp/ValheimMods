166d30448668978354c8981c389df03d888161c2
plopyy <plopyy@gmail.com>
2023-01-08 17:46:32 +0100

    Add TripleShot for Arbalest

--- EpicLoot/MagicItemEffects/MultiShot.cs
+++ EpicLoot/MagicItemEffects/MultiShot.cs
@@ -33,7 +33,7 @@ namespace EpicLoot.MagicItemEffects
                     __instance.m_projectileAccuracy = 2;
                 __instance.m_projectiles = 2;
             }
-            else if (tripleShot && itemType == ItemDrop.ItemData.ItemType.Bow && skillType == Skills.SkillType.Bows)
+            else if (tripleShot && itemType == ItemDrop.ItemData.ItemType.Bow && (skillType == Skills.SkillType.Bows || skillType == Skills.SkillType.Crossbows))
             {
                 __instance.m_projectiles = 3;
             }
