bdd0373c1534679a5ffd60eecf8073c7d2ae8062
Pete Navarra <vapokrocks@gmail.com>
2023-06-26 16:59:38 -0400

    Caught differences between my working branch and main.

--- EpicLoot/ZNet_Patch.cs
+++ EpicLoot/ZNet_Patch.cs
@@ -19,7 +19,6 @@ namespace EpicLoot
         public static void Postfix(ZNet __instance)
         {
             AdventureDataManager.OnZNetStart();
-            EnchantingTableUpgrades.OnZNetStart();
         }
     }
 
@@ -29,7 +28,6 @@ namespace EpicLoot
         public static void Postfix(ZNet __instance)
         {
             AdventureDataManager.OnZNetDestroyed();
-            EnchantingTableUpgrades.OnZNetDestroyed();
         }
     }
 
@@ -39,18 +37,7 @@ namespace EpicLoot
         public static bool Prefix(ZNet __instance)
         {
             AdventureDataManager.OnWorldSave();
-            EnchantingTableUpgrades.OnWorldSave();
             return true;
-        }
-    }
-
-    //ZoneSystem.RPC_GlobalKeys
-    [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.RPC_GlobalKeys))]
-    public static class ZoneSystem_RPC_GlobalKeys_Patch
-    {
-        public static void Postfix()
-        {
-            EnchantingTableUpgrades.NewGlobalKeys();
         }
     }
 }
