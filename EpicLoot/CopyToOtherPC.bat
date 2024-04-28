6d65f473a7e97c8e3c640ced94fe1e895a0452d8
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2024-04-27 19:06:47 -0500

    File Restructure and cleanup.
    * Restructured loose files into folders.
    * Moved all csproj references into new Paths.props file.
    * Deleted unused projects and files.

--- EpicLoot/CopyToOtherPC.bat
+++
@@ -1,5 +0,0 @@
-if exist "\\RANDY\plugins" (
-	xcopy "$(TargetDir)\$(TargetFileName)" "\\RANDY\plugins\$(ProjectName)\" /q /y /i
-    xcopy "$(ProjectDir)loottables.json" "\\RANDY\plugins\$(ProjectName)\" /q /y /i
-    xcopy "$(SolutionDir)Libs\fastJSON.dll" "\\RANDY\plugins\$(ProjectName)\" /q /y /i
-    xcopy "$(SolutionDir)ValheimUnity\AssetBundles\epicloot" "\\RANDY\plugins\$(ProjectName)\" /q /y /i )
